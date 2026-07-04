9a95a11d4a859205097799cb03128612a7d60e16
bapi kumar <bapi@example.com>
2026-07-04 17:34:30 +0530

    feat: add vote functionality to smart contract

--- contracts/poll/src/lib.rs
+++ contracts/poll/src/lib.rs
@@ -23,3 +23,38 @@ pub struct PollContract;
 
 #[contractimpl]
 impl PollContract {
+    pub fn close_poll(env: Env) {
+        env.storage().instance().set(&DataKey::Closed, &true);
+    }
+
+    pub fn vote(env: Env, voter: Address, choice: u32) -> Result<(), PollError> {
+        voter.require_auth();
+
+        if env.storage().instance().has(&DataKey::Closed) {
+            return Err(PollError::PollClosed);
+        }
+
+        if choice != 0 && choice != 1 {
+            return Err(PollError::InvalidOption);
+        }
+
+        let has_voted_key = DataKey::Voted(voter.clone());
+        if env.storage().temporary().has(&has_voted_key) {
+            return Err(PollError::AlreadyVoted);
+        }
+
+        env.storage().temporary().set(&has_voted_key, &true);
+        // Extend TTL to roughly 7 days (assuming ~5 seconds per ledger, 17280 * 7 ~ 120960 ledgers)
+        env.storage().temporary().extend_ttl(&has_voted_key, 1000, 120000);
+
+        if choice == 1 {
+            let mut yes_count: u32 = env.storage().persistent().get(&DataKey::YesCount).unwrap_or(0);
+            yes_count += 1;
+            env.storage().persistent().set(&DataKey::YesCount, &yes_count);
+            env.events().publish((symbol_short!("vote"), symbol_short!("yes")), voter);
+        } else {
+            let mut no_count: u32 = env.storage().persistent().get(&DataKey::NoCount).unwrap_or(0);
+            no_count += 1;
+            env.storage().persistent().set(&DataKey::NoCount, &no_count);
+            env.events().publish((symbol_short!("vote"), symbol_short!("no")), voter);
+        }
